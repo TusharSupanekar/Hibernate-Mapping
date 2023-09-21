377136672e361b8f74aa17f0595144533ae7f81e
TusharSupanekar <tsupanekar2810@gmail.com>
2023-09-21 10:21:45 +0530

    Mapping Techniques

---
+++ one_to_many_bi/src/main/java/com/jsp/person/account/Branch.java
@@ -0,0 +1,45 @@
+package com.jsp.person.account;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Branch {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String loc;
+
+	@ManyToOne
+	@JoinColumn
+	private Bank bank;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getLoc() {
+		return loc;
+	}
+
+	public void setLoc(String loc) {
+		this.loc = loc;
+	}
+
+	public Bank getBank() {
+		return bank;
+	}
+
+	public void setBank(Bank bank) {
+		this.bank = bank;
+	}
+
+}
---
+++ one_to_many_bi/src/main/java/com/jsp/person/account/TestSaveBankBranch.java
@@ -0,0 +1,62 @@
+package com.jsp.person.account;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class TestSaveBankBranch {
+
+	public static void main(String[] args) {
+		EntityManagerFactory entityManagerFactory =
+				Persistence.createEntityManagerFactory("tushar");
+		
+		EntityManager entityManager =
+				entityManagerFactory.createEntityManager();
+		
+		EntityTransaction entityTransaction =
+				entityManager.getTransaction();
+
+		Bank bank = new Bank();
+		bank.setName("AXIS");
+
+		Branch branch1 = new Branch();
+		branch1.setLoc("Vashi");
+
+		Branch branch2 = new Branch();
+		branch2.setLoc("Nerul");
+
+		Branch branch3 = new Branch();
+		branch3.setLoc("Kharghar");
+
+		Branch branch4 = new Branch();
+		branch4.setLoc("Sanpada");
+
+		List<Branch> branch=new ArrayList<Branch>();
+		branch.add(branch1);
+		branch.add(branch2);
+		branch.add(branch3);
+		branch.add(branch4);
+		
+		bank.setBranch(branch);
+
+		branch1.setBank(bank);
+		branch2.setBank(bank);
+		branch3.setBank(bank);
+		branch4.setBank(bank);
+		
+		
+		entityTransaction.begin();
+		entityManager.persist(bank);
+		entityManager.persist(branch1);
+		entityManager.persist(branch2);
+		entityManager.persist(branch3);
+		entityManager.persist(branch4);
+		entityTransaction.commit();
+
+	}
+
+}
